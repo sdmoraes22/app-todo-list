150cf3c5743fedae84a67189bec05013a2e43ba1
cmoraes.ti <cmoraes.ti@hotmail.com>
2018-06-26 16:16:48 -0300

    Criação do componente pageheader.

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -3,5 +3,11 @@ import React from 'react'
 export default props => (
     <div>
         <h1>Sobre</h1>
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit ammet...</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor sit ammet...</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit ammet...</p>
     </div>
 )
