9c1aae53743e1f29bf05df0b7e627923fe024435
Cristiano Moraes <cmoraes.ti@gmail.com>
2018-06-26 23:45:12 -0300

    Criação de evento para atualização de componente controlado

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -7,18 +7,26 @@ export default class Todo extends Component {
     constructor(props) {
         super(props)
 
+        this.state = {description: '', list: []}
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(e) {
+        this.setState({...this.state, description: e.target.value})
     }
 
     handleAdd() {
-        console.log(this)
+        console.log(this.state.description)
     }
 
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro' />
-                <TodoForm handleAdd={this.handleAdd} />
+                <TodoForm description={this.state.description}
+                handleChange={this.handleChange}
+                handleAdd={this.handleAdd} />
                 <TodoList />
             </div>
         )
