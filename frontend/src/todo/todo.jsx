af5a4e71e43431a576ec76419fcafb8b485fd179
Cristiano Moraes <cmoraes.ti@gmail.com>
2018-06-26 23:23:23 -0300

    Inserido evento e método de vericação de evento na TodoForm

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -4,11 +4,21 @@ import TodoForm from './todoform'
 import TodoList from './todoList'
 
 export default class Todo extends Component {
+    constructor(props) {
+        super(props)
+
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    handleAdd() {
+        console.log(this)
+    }
+
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro' />
-                <TodoForm />
+                <TodoForm handleAdd={this.handleAdd} />
                 <TodoList />
             </div>
         )
