0a70aa10022e6afd722af6c5fb7ae5c0265b5ca0
Cristiano Moraes <cmoraes.ti@gmail.com>
2018-08-26 11:21:08 -0300

    Adicionando a ação de limpeza de formulários

--- frontend/src/todo/todoform.jsx
+++ frontend/src/todo/todoform.jsx
@@ -5,7 +5,7 @@ import { bindActionCreators } from 'redux'
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
 
-import { add, changeDescription, search } from './todoActions'
+import { add, changeDescription, search, clear } from './todoActions'
 
 class TodoForm extends Component {
     constructor(props) {
@@ -18,17 +18,17 @@ class TodoForm extends Component {
     }
 
     keyHandler(e) {
-        const { add, search, description } = this.props
+        const { add, search, description, clear } = this.props
         if (e.key === 'Enter') {
             e.shiftKey ? search() : add(description)
         } else if (e.key === 'Escape') {
-            this.props.handleClear()
+            clear()
         }
     }
 
     
     render() {
-        const { add, search, description } = this.props
+        const { add, search, description, clear } = this.props
         return (
             <div role='form' className='todoForm'>
                 <Grid cols='12 9 10'>
@@ -45,7 +45,7 @@ class TodoForm extends Component {
                     <IconButton style='info' icon='search'
                         onClick={() => search()}></IconButton>
                     <IconButton style='default' icon='close'
-                        onClick={this.props.handleClear}></IconButton>
+                        onClick={clear}></IconButton>
                 </Grid>
             </div>
         )
@@ -55,6 +55,6 @@ class TodoForm extends Component {
 
 const mapStateToProps = state => ({description: state.todo.description})
 const mapDispatchToPros = dispatch => 
-        bindActionCreators({ add, changeDescription, search }, dispatch)
+        bindActionCreators({ add, changeDescription, search, clear }, dispatch)
 
 export default connect(mapStateToProps, mapDispatchToPros)(TodoForm)
